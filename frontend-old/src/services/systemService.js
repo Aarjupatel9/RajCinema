bb7edda2d819ec62683db0c99d90619e792caa2a
Aarju Patel <aarjupatel922003@gmail.com>
2023-10-07 17:36:22 +0530

    init

--- frontend-old/src/services/systemService.js
+++
@@ -1,9 +0,0 @@
-import authService from "./auth.service";
-export function handleRejectResponse(message) {
-    console.log("handleRejectResponse called : ",message);
-    if (message == "TokenExpiredError") {
-        console.log("handleRejectResponse called inside");
-        authService.logout();
-        window.location.reload();
-    }
-}
